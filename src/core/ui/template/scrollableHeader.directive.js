6396c943bccd500ce06977849be5987e723ca415
htz <htz@worktile.com>
2017-03-20 17:00:38 +0800

    fix header style width, align at scroll.

--- src/core/ui/template/scrollableHeader.directive.js
+++ src/core/ui/template/scrollableHeader.directive.js
@@ -9,14 +9,15 @@
                 var css = {};
 
                 var maxHeightActivated = $scope.gantt.scroll.isVScrollbarVisible();
-                var vScrollbarWidth = maxHeightActivated ? scrollBarWidth: 0;
+                // var vScrollbarWidth = maxHeightActivated ? scrollBarWidth: 0;
                 var columnWidth = this.gantt.options.value('columnWidth');
                 var bodySmallerThanGantt = $scope.gantt.width === 0 ? false: $scope.gantt.width < $scope.gantt.getWidth() - $scope.gantt.side.getWidth();
 
                 if (columnWidth !== undefined && bodySmallerThanGantt) {
                     css.width = ($scope.gantt.width - vScrollbarWidth + this.gantt.scroll.getBordersWidth()) + 'px';
                 } else if (maxHeightActivated) {
-                    css.width = $scope.gantt.getWidth() - $scope.gantt.side.getWidth() - vScrollbarWidth + 'px';
+                    css.width = $scope.gantt.getWidth() - $scope.gantt.side.getWidth() + 'px';
+                    // css.width = $scope.gantt.getWidth() - $scope.gantt.side.getWidth() - vScrollbarWidth + 'px';
                 }
 
                 /*
